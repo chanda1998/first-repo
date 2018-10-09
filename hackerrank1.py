3c1bce9aaf4a03b3333defd492a216b9ba842fe3
chanda1998 <jhachandan78277@gmail.com>
2018-10-09 22:12:11 +0530

    Update hackerrank1.py

--- hackerrank1.py
+++ hackerrank1.py
@@ -1,7 +1,21 @@
-#!/bin/python3
+# split string into len/k subsegments each subsegment has n characters where n = len/k
 
-if __name__ == '__main__':
-    a = int(input())
+string = "AABCAAADA"
+string1 = "chandananjanjsbfvkhsbkwubohfvbwbvibuwbbvux" # len = 42
+k = 3
 
-for i in range(a):
-    print(i+1, end='')
+
+def merge_the_tools(stri, k):
+    n = int(len(stri)/k)        # n = 42/6 = 7
+    lt = []
+    for i in range(k):
+        print(stri[i*n:i*n+n])      # n-k = 7-6 = 1  n*i = 7*0=0 7*1 = 7
+        # lt.append(''.join(sorted(set(stri[n-k:n]), key=stri[n-k:n].index)))
+        n += k
+
+
+
+
+
+
+merge_the_tools(string1, 6)
